e836407e120cf9d6d5af08d8e0004b23bb2ec705
s20g470 <s20g470@stu.kagawa-u.ac.jp>
2020-12-03 23:45:47 +0900

    挨拶

---
+++ builder/page/30808.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset='utf-8'>
+	<title>i13302/SLPアドベントカレンダー2020</title>
+	<link rel="stylesheet" type="text/css" href="style/main.css">
+	<link rel="stylesheet" type="text/css" href="style/sidebar.css">
+	<link rel="stylesheet" type="text/css" href="style/object.css">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+	<div class=contents>
+<?php include('./parts/nav.php'); ?>
+
+		<article>
+			<section>
+				<h1>SLPアドベントカレンダー2020</h1>
+
+				<p>この記事は，<a href='https://adventar.org/calendars/5402'>SLP KBIT Advent Calendar 2020 - Adventar </a> の5日目(12月5日)の記事です．</p>
+				<p>今回は，自分のHPで紹介します．<br>
+					なかなかにシュールでごめんなさい．<br>
+					実は別の名前で去年(2019年)もやっていますので，探して見てください</p>
+			</section>
+		</article>
+	</div>
+	<header>
+		header
+	</header>
+	<footer>
+		footer
+	</footer>
+</body>
+
+</html>
