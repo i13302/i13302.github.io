011bc777d6e5a65d3068f0681703941b97760733
s20g470 <s20g470@stu.kagawa-u.ac.jp>
2020-12-08 13:41:38 +0900

    紹介の途中まで

--- builder/page/30808.php
+++ builder/page/30808.php
@@ -18,13 +18,26 @@
 			<section>
 				<h1>SLPアドベントカレンダー2020</h1>
 				<h2>はじめに</h2>
-				<p>この記事は，<a href='https://adventar.org/calendars/5402'>SLP KBIT Advent Calendar 2020 - Adventar</a> の5日目(12月5日)の記事です．</p>
-				<p>今回は，自分のHPで紹介します．<br>
-					なかなかにシュールでごめんなさい．</p>
-				<p>本日は，GitHub ActionsとDockerfileを用いたLaTeXのビルド方法を紹介します．</p>
+				<p>この記事は，<a href='https://adventar.org/calendars/5402'>SLP KBIT Advent Calendar 2020 - Adventar</a> の5日目(12月5日)の記事です．<br>
+				公開が遅くなり，ごめんなさい．</p>
+				
 				<p>今までの日を見ますと，皆様かなり本格的な内容を紹介していますね．<br>
 				本日紹介する内容は，技術的にはそこまで大したものではありません．<br>
 				ただ，実際に使って便利だったので紹介する感じです．</p>
+				
+				<h2>紹介</h2>
+				<p>リポジトリは <a href='https://github.com/i13302/latex-actions'>i13302/latex-actions</a> です．</p>
+				
+				<p>本日は，GitHub ActionsとDockerfileを用いたLaTeXのビルドテンプレートを紹介します．</p>
+				
+				<p>12月にもなると，卒論や修論，はたまた学会の全国大会論文の締切が現実味を帯びてきますよね．<br>
+				理系の学生は，LaTeXで論文を執筆し，Git管理する人が多いかと思います(偏見！)．<br>
+				しかし，以下の問題点を感じる人が多いかと思います．<br>
+				<ul>
+					<li>現在の最新版はビルドできるのか．</li>
+					<li>第2著者が書いた部分はビルドできるのか．</li>
+					<li>論文PDFが欲しいとき，プロジェクトをCloneしてビルドするのが面倒．</li>
+				</ul>
 				
 			</section>
 		</article>
