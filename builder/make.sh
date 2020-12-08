03feeb05de5bc12c4caf32254978e7c8296721f6
s20g470 <s20g470@stu.kagawa-u.ac.jp>
2020-12-08 16:07:20 +0900

    リスト操作に苦戦

--- builder/make.sh
+++ builder/make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-PAGEFILE=`cat ./files.txt`
+FILESDATA=`cat ./files.txt`
+PAGEFILE=(`cat ./files.txt | cut -f 1`)
+PAGETITLE=(`cat ./files.txt | cut -f 2`)
 ROOTPATH='../..'
 
 # PHPファイルを基にHTMLファイルを生成
@@ -23,6 +25,27 @@ function clean(){
 function newfile(){
 	cp parts/_template.php $1.php
 	echo $1 >> $ROOTPATH/builder/files.txt
+}
+	
+	#	<nav>
+	#		<ul>
+	#			<li><a href=index.html>トップページ</a></li>
+	#			<li><a href=achieve.html>実績一覧</a></li>
+	#			<li><a href=project.html>開発物</a></li>
+	#		</ul>
+	#	</nav>
+
+function create(){
+	if [ $1 == nav ]
+	then 
+		echo "<nav><ul>" > $ROOTPATH/builder/page/parts/nav.php
+		echo "" >> $ROOTPATH/builder/page/parts/nav.php
+		echo  ${#PAGEFILE[@]}
+		for i in `seq 0 ${#PAGEFILE[@]}`
+		do
+			echo $PAGEFILE[$i]
+		done
+	fi
 }
 
 
