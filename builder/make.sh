63154f320d4656ff2045488ef524d80adb41d983
i13302 <s20g470@stu.kagawa-u.ac.jp>
2020-12-13 20:17:45 +0900

    h2タグを目次に落とす

--- builder/make.sh
+++ builder/make.sh
@@ -25,6 +25,20 @@ function newfile(){
 	echo $1 >> $ROOTPATH/builder/files.txt
 }
 
+# 目次を生成
+function table_of_contents(){
+	mkdir -p parts/table_of_contents
+	echo '<ul>' > parts/table_of_contents/$1.php
+	for _h2 in $(cat $1.php |\
+		grep -i '<h2>' |\
+		sed -e 's/<[^>]*>//g' |\
+		sed -e 's/\t*//g')
+	do
+		echo '<li>'$_h2'</li>' >> parts/table_of_contents/$1.php
+	done
+	echo '</ul>' >> parts/table_of_contents/$1.php
+}
+
 
 # echo $PAGEFILE
 cd page
