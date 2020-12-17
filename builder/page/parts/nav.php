b5721bd9eedd835a0f3c779c6d64666aa60fc8cd
s20g470 <s20g470@stu.kagawa-u.ac.jp>
2020-12-17 14:29:37 +0900

    リリースするhtmlファイルの場所を変更

--- builder/page/parts/nav.php
+++ builder/page/parts/nav.php
@@ -1,6 +1,6 @@
 <nav><ul>
-<li><a href=index.html>index</a></li>
-<li><a href=achieve.html>achieve</a></li>
-<li><a href=project.html>project</a></li>
-<li><a href=30808.html>30808</a></li>
+<li><a href=index.html>トップページ</a></li>
+<li><a href=achieve.html>実績一覧</a></li>
+<li><a href=project.html>制作物</a></li>
+<li><a href=30808.html>SLPアドカレ2020</a></li>
 </ul></nav>
