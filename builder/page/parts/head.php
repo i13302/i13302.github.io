cd2cb7ef174e03fe56f2be4560c11a31a37c1847
s20g470 <s20g470@stu.kagawa-u.ac.jp>
2021-03-03 16:33:08 +0900

    fix #23

--- builder/page/parts/head.php
+++ builder/page/parts/head.php
@@ -1,7 +1,6 @@
 <?php
 function load_head($title){
 ?>
-	<meta charset='utf-8'>
 	<meta name="viewport" content="width=device-width">
 	<title>i13302/<?= $title ?></title>
 	<link rel="stylesheet" type="text/css" href="style/main.css">
