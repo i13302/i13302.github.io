0cead6639066a8ba57be621f090a3ec516aff8b4
i13302 <s20g470@stu.kagawa-u.ac.jp>
2021-05-08 02:21:40 +0900

    TA実績を別ページに

---
+++ builder/page/610.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+
+<head>
+<?php include('./parts/head.php'); load_head('TA実績');?>
+</head>
+
+<body>
+	<div class=contents>
+<?php include('./parts/nav.php'); ?>
+
+		<article>
+			<section>
+			<h1>TA担当科目</h1>
+			<h2 class=bottom0>2016年度(香川高専)</h2>
+			<ul class=tb00>
+				<li><span class=mgright>(高専2年対象)</span><a target='_blank' href='https://www.kagawa-nct.ac.jp/school_affairs/syllabus/takuma/2016/pdf/IT/16237006.pdf'>基礎工学実験・実習</a></li>
+			</ul>
+			<h2 class=bottom0>2017年度(香川高専)</h2>
+			<ul class=tb00>
+				<li><span class=mgright>(高専2年対象)</span><a target='_blank' href='https://www.kagawa-nct.ac.jp/school_affairs/syllabus/takuma/2017/pdf/IT/17237006.pdf'>基礎工学実験・実習</a></li>
+			</ul>
+			<h2 class=bottom0>2018年度(香川大学)</h2>
+			<ul class=tb00>
+				<li><span class=mgright>(学部1年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2018&lct_cd=5000533'>プログラミング (情シス・通信)</a></li>
+				<li><span class=mgright>(学部2年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2018&lct_cd=551570'>電子・情報工学プログラミング</a></li>
+			</ul>
+			<h2 class=bottom0>2019年度(香川大学)</h2>
+			<ul class=tb00>
+				<li><span class=mgright>(学部2年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2019&lct_cd=5005030'>中級プログラミング</a></li>
+				<li><span class=mgright>(学部3年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2019&lct_cd=577500'>情報環境実験I</a></li>
+			</ul>
+			<h2 class=bottom0>2020年度(香川大学)</h2>
+			<ul class=tb00>
+				<li><span class=mgright>(学部1年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2020&lct_cd=5000533'>プログラミング (情シス・通信)</a></li>
+				<li><span class=mgright>(学部2年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2020&lct_cd=5005030'>中級プログラミング</a></li>
+				<li><span class=mgright>(学部2年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2020&lct_cd=5005080'>アルゴリズム演習</a></li>
+				<li><span class=mgright>(学部3年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2020&lct_cd=5005310'>情報システム・セキュリティ実験I</a></li>
+				<li><span class=mgright>(学部3年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2020&lct_cd=5005320'>情報システム・セキュリティ実験II</a></li>
+				<li><span class=mgright>(学部3年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2020&lct_cd=5005140'>情報セキュリティII</a></li>
+				<li><span class=mgright>(学部3年対象)</span><a target='_blank' href='https://www2.st.kagawa-u.ac.jp/Portal/Public/Syllabus/DetailMain.aspx?lct_year=2020&lct_cd=5005150'>情報セキュリティ演習</a></li>
+			</ul>
+			</section>
+		</article>
+	</div>
+	<header>
+		<!-- header -->
+	</header>
+	<footer>
+		<!-- footer -->
+	</footer>
+</body>
+
+</html>
