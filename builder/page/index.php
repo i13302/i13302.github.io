19f162a65c179814f7d4f70c44c6f2a456e665eb
i13302 <s20g470@stu.kagawa-u.ac.jp>
2021-05-08 20:52:40 +0900

    GitHubのlangをcompactに

--- builder/page/index.php
+++ builder/page/index.php
@@ -73,7 +73,7 @@
 				<h2>GitHubの活動状況</h2>
 				<img src='https://github-profile-trophy.vercel.app/?username=i13302'><br>
 				<img src='https://github-readme-stats.vercel.app/api?username=i13302&show_icons=true'>
-				<img src='https://github-readme-stats.vercel.app/api/top-langs/?username=i13302'>
+				<img src='https://github-readme-stats.vercel.app/api/top-langs/?username=i13302&layout=compact'>
 				<hr>
 			</section>
 		</article>
